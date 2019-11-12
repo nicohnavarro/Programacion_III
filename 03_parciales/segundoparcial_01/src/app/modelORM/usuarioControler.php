89fcfa431162a6261fb20412483e6f373bb78abf
nicohnavarro <nicohnavarro@gmail.com>
2019-11-12 20:39:42 -0300

    GRAL - Pre-Exam 2

---
+++ 03_parciales/segundoparcial_01/src/app/modelORM/usuarioControler.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Models\ORM;
+use App\Models\ORM\usuario;
+use App\Models\IApiControler;
+
+include_once __DIR__ . '/usuario.php';
+include_once __DIR__ . '../../modelAPI/IApiControler.php';
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+
+class usuarioControler implements IApiControler 
+{
+ 	public function Beinvenida($request, $response, $args) {
+      $response->getBody()->write("GET => Bienvenido!!! ,a UTN FRA SlimFramework TOMA PAVA VO");
+    
+    return $response;
+    }
+}
